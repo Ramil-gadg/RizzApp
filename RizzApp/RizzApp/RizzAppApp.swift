9d96c303e43c2a9a28112b0adb50d1bc83564a43
Гаджиев Рамил Имранович <RImGadzhiev@sberbank.ru>
2025-03-12 20:59:01 +0300

    initial

---
+++ RizzApp/RizzApp/RizzAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RizzAppApp.swift
+//  RizzApp
+//
+//  Created by Гаджиев Рамил Имранович on 12.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct RizzAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
